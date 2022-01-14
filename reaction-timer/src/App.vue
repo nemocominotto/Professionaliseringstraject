fcc47b27a91f8c52004674590814337c51fdc199
nemocominotto <r0797042@student.thomasmore.be>
2022-01-14 12:55:00 +0100

    Reaction timer

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying" >Play</button>
+    <Block v-if="isPlaying" :delay="delay" @stop="endGame" />
+    <Result v-if="showResult" :score="score" />
+  </div>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Result
+  },
+  data(){
+    return {
+      isPlaying: false,
+      showResult: false,
+      delay: null,
+      score: null
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.showResult = false
+      this.isPlaying = true
+      //console.log(this.delay)
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.showResult = true
+      this.isPlaying = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Reaction scrore: {{score}} ms</p>
+    <p class="rank">{{rank}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if (this.score < 250) {
+            this.rank = 'Fast and furious!!'
+        } else if (this.score < 400) {
+            this.rank = 'Fast'
+        } else {
+            this.rank = 'Slow...'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
