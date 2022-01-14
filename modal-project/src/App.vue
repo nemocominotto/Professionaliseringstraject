7a7b30fe11d96c9586a9b6f5b97f196b47075f3a
nemocominotto <r0797042@student.thomasmore.be>
2022-01-14 10:52:04 +0100

    Modal project

---
+++ modal-project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div v-if="showModel">
+      <Modal :header="header" @close="toggleModel" />
+    </div>
+    <button @click="toggleModel">open model</button>
+  </div>
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data(){
+    return {
+      title: 'My first app',
+      header: 'My first component',
+      showModel: false
+    }
+  },
+  methods: {
+    toggleModel() {
+      this.showModel = !this.showModel
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+            <h1>{{ header }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: lightcyan;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
+  h1 {
+      color: darkblue;
+  }
+</style>
